842da1155b9484762f0c8b86c0ef7961a6654141
Finley Sherwood <me@itsf.in>
2021-12-31 11:18:31 +1000

    Restart project using nextjs

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  // 'rules' to add or remove checks
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'no-var': 2,
+    'no-undef': 2,
+    'no-param-reassign': 2,
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app'
+import '../styles/globals.sass'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
