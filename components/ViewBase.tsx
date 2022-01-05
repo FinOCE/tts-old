586c077ad59292047aeb80176e0c33ad9f0cffba
Finley Sherwood <me@itsf.in>
2022-01-05 13:02:22 +1000

    Add music

--- components/ViewBase.tsx
+++ components/ViewBase.tsx
@@ -1,3 +1,4 @@
+import Sound from 'react-sound'
 import styles from '../styles/ViewBase.module.sass'
 
 export default function ViewBase(props: { children: JSX.Element }) {
@@ -7,6 +8,13 @@ export default function ViewBase(props: { children: JSX.Element }) {
         <video playsInline autoPlay muted loop>
           <source src={'milky_way.mp4'} type={'video/mp4'} />
         </video>
+        <Sound
+          url={'lovely.mp3'}
+          loop
+          autoLoad
+          volume={25}
+          playStatus={'PLAYING'}
+        />
       </div>
       <div id={styles.content}>{props.children}</div>
     </main>
